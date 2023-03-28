6935df9ab89e5539594dbe2c587058360f0aa3c9
Veronica Maciel <vevemacielmaciel@gmail.com>
2023-03-28 19:25:26 -0300

    exemplos da aula 1 - semana 9

---
+++ type-boolean.js
@@ -0,0 +1,10 @@
+const primeiroNum = 5;
+const segundoNum = 10;
+const cadastroAtivado = true;
+
+//console.log(primeiroNum != segundoNum);
+
+const texto1 = 'alura';
+const texto2 = 'Alura';
+
+console.log (texto1== texto2)
---
+++ type-string.js
@@ -0,0 +1,21 @@
+// tipo number
+const  texto1=  'ola mundo' ;
+const  texto3=  "ola mundo" ;
+const senha='senhSuperSegura';
+const StringDeNumeros = "34567";
+
+const citacao = ' Meu nome é ';
+const meuNome = 'leonardo'
+console.log(citacao + meuNome)
+
+//concatenação (+)
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
